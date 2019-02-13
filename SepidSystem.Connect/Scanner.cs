efb1598b7d575c79fa4d8ff56e1a07e3275a7176
rasoulian <arasoulian@gmail.com>
2019-02-13 18:12:35 +0330

    add some idea

---
+++ SepidSystem.Connect/Scanner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SepidSystem.Connect
+{
+    public interface IScanner
+    {
+        byte[] Scan();
+    }
+
+    public class Scanner : IScanner
+    {
+        public byte[] Scan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
